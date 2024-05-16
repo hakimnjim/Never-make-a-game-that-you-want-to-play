e26f9ef4a9f6a43dce74fc020f56a8f40b728962
Hakim Njim <njim.hakimnjim1992@gmail.com>
2024-05-16 17:37:45 +0100

    Add card rotation

--- Made with love/Assets/Scripts/Card.cs
+++ Made with love/Assets/Scripts/Card.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,13 @@ public class Card : MonoBehaviour, ICard
     public bool FlipInProgress { get => flipInProgress; set => flipInProgress = value; }
     public bool IsFacingUp { get => isFacingUp; set => isFacingUp = value; }
 
+    private Ease flipEase = Ease.OutQuad;
+
+    [SerializeField] private SpriteRenderer currentCard;
+    [SerializeField] private Sprite fontFace;
+    [SerializeField] private Sprite backFace;
+    [SerializeField] private float flipDuration = 0.5f;
+
     public void Init(CardStruct cardStruct)
     {
        
@@ -16,7 +24,27 @@ public class Card : MonoBehaviour, ICard
 
     public void Rotate(bool forceRotation = false)
     {
-        
+        if ((flipInProgress || IsFacingUp) && !forceRotation)
+        {
+            return;
+        }
+        float duration = 0;
+        Quaternion targetRotation = IsFacingUp ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
+        IsFacingUp = !IsFacingUp;
+        transform.DORotateQuaternion(targetRotation, flipDuration)
+            .SetEase(flipEase).OnUpdate(() =>
+            {
+                FlipInProgress = true;
+                duration += Time.deltaTime;
+                if (duration >= flipDuration / 2)
+                {
+                    currentCard.sprite = IsFacingUp ? fontFace : backFace;
+                }
+            })
+            .OnComplete(() =>
+            {
+                FlipInProgress = false;
+            });
     }
 
     private void OnMouseDown()
@@ -26,6 +54,7 @@ public class Card : MonoBehaviour, ICard
 
     private void OnMouseUp()
     {
+        Rotate();
         Debug.Log("hey we select this card " + this.gameObject.name);
     }
 
